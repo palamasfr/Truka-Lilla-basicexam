7b6e3f7bbe06d0fcd307a689a93abe485190fa7f
Lilla Truka <trukacsa@gmail.com>
2018-07-09 07:27:04 +0200

    fixing

--- app.js
+++ app.js
@@ -1,25 +1,5 @@
 // ide deklaráljátok a függvényeket.
 
-function getData(url, callbackFunc) {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      callbackFunc(this);
-    }
-  };
-  xhttp.open('GET', url, true);
-  xhttp.send();
-}
-
-function successAjax(xhttp) {
-  // Innen lesz elérhető a JSON file tartalma, tehát az adatok amikkel dolgoznod kell
-  var userDatas = JSON.parse(xhttp.responseText);
-  // Innen lehet hívni.
-  console.log(userDatas);
-}
-getData('/json/spaceships.json', successAjax);
-
-
 function orderByCost(successAjax) {
 
   var i = userDatas.length - 1;
@@ -125,3 +105,24 @@ function searchByName(inputName, userDatas) {
   }
   console.log(inputName);
 };
+
+
+function getData(url, callbackFunc) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      callbackFunc(this);
+    }
+  };
+  xhttp.open('GET', url, true);
+  xhttp.send();
+}
+
+function successAjax(xhttp) {
+  // Innen lesz elérhető a JSON file tartalma, tehát az adatok amikkel dolgoznod kell
+  var userDatas = JSON.parse(xhttp.responseText);
+  // Innen lehet hívni.
+  console.log(userDatas);
+}
+getData('/json/spaceships.json', successAjax);
+
